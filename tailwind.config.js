c2bb7da22c0173ca1cbdb5fdfa3a74351878b389
Marco <marcolunardi10@gmail.com>
2024-06-25 16:00:02 -0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,40 @@
 module.exports = {
-mode: "jit",
-content: ["./src/**/**/*.{js,ts, jsx, tsx, html, mdx}", "./src/**/*.{js,ts,jsx, tsx, html, mdx}"], 
-darkMode: "class",
-theme: {
-  screens: { md: { max: "1050px" }, sm: { max: "550px" } },
-  extend: {
-    colors: {
-    black: {"900_33": "#00000033", "900_3f": "#0000003f", "900_5f": "#0000005f" },
-    gray: {300: "#dddfeb" },
-    indigo: {700: "#344789", 800: "#344689", 900: "#001489", "700_01": "#38448a", "800_01": "#37438a" }, 
-    neutral: { neutral_0: "#ffffffbf" },
-    red: { a700: "#d51515" },
-    white: { a700: "#ffffff" },
+  mode: "jit",
+  content: ["./src/**/**/*.{js,ts,jsx,tsx,html,mdx}", "./src/**/*.{js,ts,jsx,tsx,html,mdx}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      md: { max: "1050px" },
+      sm: { max: "550px" }
     },
-    boxShadow: { xs: "4px 8px 4px 0 #0000003f", sm: "4px 8px 4px 0 #0000005f", md: "4px 8px 4px 0 #00000033" }, 
-    fontFamily: { poppins: "Poppins", paytoneone: "Paytone One", inter: "Inter" },
-},  
-},
-  plugins: [require("@tailwindcss/forms")],
+    extend: {
+      colors: {
+        black: "#000000",
+        gray: { 300: "#dddfeb" },
+        indigo: {
+          700: "#344789",
+          800: "#344689",
+          900: "#001489",
+          "700_01": "#38448a",
+          "800_01": "#37438a"
+        },
+        neutral: { neutral_0: "#ffffffbf" },
+        red: { a700: "#d51515" },
+        white: { a700: "#ffffff" }
+      },
+      boxShadow: {
+        xs: "4px 8px 4px 0 rgba(0,0,0,0.3)",
+        sm: "4px 8px 4px 0 rgba(0,0,0,0.5)",
+        md: "4px 8px 4px 0 rgba(0,0,0,0.2)"
+      },
+      fontFamily: {
+        poppins: "Poppins",
+        paytoneone: "Paytone One",
+        inter: "Inter"
+      }
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms")
+  ]
 };
